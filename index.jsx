4e78f8672f3bf7751c8b943dd16e21c2458aff06
Julia Breitenbruch <119942939+JBreitenbr@users.noreply.github.com>
2025-09-30 11:00:25 +0200

    Update index.jsx

--- index.jsx
+++ index.jsx
@@ -9,6 +9,21 @@ return (<div className="flex flex-row mx-2 mt-4 justify-center items-center" sty
 </div>)
 };
 
+export const SongTitles=({item})=>{
+  let songs=item.album_tracks.split(")/");
+  let med=Math.floor(songs.length/2);
+  let songs1=songs.slice(0,med+songs.length%2);
+  let songs2=songs.slice(med+songs.length%2); 
+  return (<div className="grid grid-cols-2 px-6 py-4" style={{maxWidth:"80vw"}}>
+      <div>
+       <ol>{songs1.map((track)=><li className="text-wrap text-xs w-32">{track+")"}</li>)}</ol>
+      </div>
+      <div className="text-wrap flex">
+         <ol className="ml-4" start={med+songs.length%2+1}>{songs2.map((track)=><li className="text-wrap text-xs w-32">{track+")"}</li>)}</ol>
+      </div>
+  </div>);
+};
+
 export const Footer = () => {
   return (<footer><p>© Julia Breitenbruch</p>
   </footer>)
