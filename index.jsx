106db6d42795545015e29278ba41ea1085c9b063
Julia Breitenbruch <119942939+JBreitenbr@users.noreply.github.com>
2025-09-30 10:44:45 +0200

    Update and rename index.jsx to Footer.jsx

--- index.jsx
+++
@@ -1,4 +0,0 @@
-export const Footer = () => {
-  return (<footer><ul><li>Fitness Dashboard</li><li>Watch Videos</li></ul><ul><li>Privacy Policy</li><li><a href="#">Services</a></li></ul><ul><li><a href="#">Discord</a></li><li><a href="#">Terms and Conditions</a></li></ul><p>© 2024 Fitness Dashboard. All Rights Reserved.</p>
-  </footer>)
-};
