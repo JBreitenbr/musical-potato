5dec77d4baa80582d61c7f125f4e8fc74db42222
Julia Breitenbruch <119942939+JBreitenbr@users.noreply.github.com>
2025-10-04 10:21:30 +0200

    Update index.jsx

--- index.jsx
+++ index.jsx
@@ -24,6 +24,18 @@ export const SongTitles=({item})=>{
   </div>);
 };
 
+export const TrackList = ({ tracks }) => {
+  return (<ul className="list-group grid grid-cols-2 mt-4">
+      {tracks.map((track,index) => (
+        <li key={track.track}><div className="ml-4 mb-4">
+          {/*<Tooltip id={`my-anchor-element-${index}`}><div style={{overflowY:"scroll",maxHeight:"60vh"}}><AlbumInfo item={track}/><SongTitles item={track}/></div></Tooltip><a data-tooltip-id={`my-anchor-element-${index}`}>
+*/} <img src={track.album_pic} className="h-16 w-16 sm:h-32 sm:w-32" style={{border:"1px solid #0f172a"}}/>{/*</a>*/}
+<div className="sm:text-xl md:text-2xl xl:text-3xl mt-2 text-white" style={{fontSize:"0.84rem"}}>{track.track}</div></div></li>
+      ))}
+    </ul>
+  );
+};
+
 export const Footer = () => {
   return (<footer><p>© Julia Breitenbruch</p>
   </footer>)
